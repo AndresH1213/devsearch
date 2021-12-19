bb91ccd3843ba9d80fce200127fc16cfc6a37d43
Andres Hernandez <edanhebla1213@hotmail.com>
2021-12-19 11:42:27 -0500

    django rest framework and serializers

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from functools import partial
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.getRoutes),
+    path('projects/', views.getProjects),
+    path('projects/<str:pk>/', views.getProject)
+]
